913e33c7225e298f240951c6b71ce7bb3b14c59a
Pierre <z5482784@ad.unsw.edu.au>
2025-02-07 21:22:38 +1100

    deployed using Vite, vite vanilla JavaScript with Three.js

---
+++ src/main.js
@@ -0,0 +1,66 @@
+import './style.css'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer  = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
+
+const geometry = new THREE.SphereGeometry( 15, 32, 16 ); 
+const material = new THREE.MeshStandardMaterial( { color: 0xffff00 } ); 
+const sphere = new THREE.Mesh( geometry, material ); 
+
+scene.add( sphere );
+
+const pointLight = new THREE.PointLight( 0xffffff )
+pointLight.position.set(5,5,5)
+
+const ambientLight = new THREE.AmbientLight( 0xffffff )
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(pointLight, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.IcosahedronGeometry(0.15,0)
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } )
+  const star = new THREE.Mesh( geometry, material );
+
+  const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 200 ));
+  star.position.set(x,y,z);
+  scene.add(star)
+}
+
+Array(150).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load( 'space.jpg');
+scene.background = spaceTexture;
+
+
+// recursive function, "game loop" when called will reset the UI
+function animate () {
+  requestAnimationFrame ( animate );
+  sphere.rotation.x -= 0.001;
+  sphere.rotation.y += 0.01;
+  sphere.rotation.z -= 0.00001;
+
+  controls.update();
+
+  renderer.render( scene, camera );
+}
+
+animate();
